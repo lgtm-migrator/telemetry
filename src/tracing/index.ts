4e1f13a20596384518e96986612fd5d9d7f9b962
Ofer <12687466+CptSchnitz@users.noreply.github.com>
2022-06-13 10:48:24 +0300

    feat: added functions to simplify ignoring request paths (#48)
    * feat: added functions to simplify ignoring request paths
    
    * fix: wrong condition

--- src/tracing/index.ts
+++ src/tracing/index.ts
@@ -1,5 +1,5 @@
 export { Tracing } from './tracing';
 export { getTraceContexHeaderMiddleware } from './middleware/traceOnHeaderMiddleware';
-export { contexBindingHelper } from './utils/tracing';
+export { contexBindingHelper, ignoreIncomingRequestUrl, ignoreOutgoingRequestPath } from './utils/tracing';
 export { getOtelMixin } from './mixin';
 export { logMethod } from './loggerHook';
--- src/tracing/utils/tracing.ts
+++ src/tracing/utils/tracing.ts
@@ -1,6 +1,15 @@
+import { IncomingMessage, RequestOptions } from 'http';
 import api, { Span } from '@opentelemetry/api';
 
 export const contexBindingHelper = <T>(parentSpan: Span, func: T): T => {
   const ctx = api.trace.setSpan(api.context.active(), parentSpan);
   return api.context.bind(ctx, func);
+};
+
+export const ignoreIncomingRequestUrl = (urlsToIgnore: RegExp[]): ((request: IncomingMessage) => boolean) => {
+  return (request): boolean => urlsToIgnore.some((regex) => regex.test(request.url ?? ''));
+};
+
+export const ignoreOutgoingRequestPath = (pathsToIgnore: RegExp[]): ((request: RequestOptions) => boolean) => {
+  return (request): boolean => pathsToIgnore.some((regex) => regex.test(request.path ?? ''));
 };
